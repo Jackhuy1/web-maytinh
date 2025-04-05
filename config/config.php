88efadf31c2bb888058d34744f53c0265fa8f4e0
Jackhuy1 <91598544+Jackhuy1@users.noreply.github.com>
2025-04-05 11:53:09 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
-$configDB["database"]	= "computer_store";
-$configDB["username"] 	= "root";
-$configDB["password"] 	= "";
-define("HOST", "localhost");
-define("DB_NAME", "computer_store");
-define("DB_USER", "root");
-define("DB_PASS", "");
+$configDB["host"] 		= "maytinhsql.mysql.database.azure.com";
+$configDB["database"]	= "maytinh";
+$configDB["username"] 	= "sqluser";
+$configDB["password"] 	= "P@sswrd45";
+define("HOST", "maytinhsql.mysql.database.azure.com");
+define("DB_NAME", "maytinh");
+define("DB_USER", "sqluser");
+define("DB_PASS", "P@ssword45");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
