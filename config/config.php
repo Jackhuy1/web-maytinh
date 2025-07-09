a184f106d997e6248b66a487040554ef3c395984
Jackhuy1 <91598544+Jackhuy1@users.noreply.github.com>
2025-07-09 15:39:25 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "tamldmysql.mysql.database.azure.com";
+$configDB["host"] 		= "localhost";
 $configDB["database"]	= "maytinh";
-$configDB["username"] 	= "tamld";
+$configDB["username"] 	= "maytinh";
 $configDB["password"] 	= "Tdc@12345";
-define("HOST", "tamldmysql.mysql.database.azure.com");
+define("HOST", "localhost");
 define("DB_NAME", "maytinh");
-define("DB_USER", "tamld");
+define("DB_USER", "maytinh");
 define("DB_PASS", "Tdc@12345");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
